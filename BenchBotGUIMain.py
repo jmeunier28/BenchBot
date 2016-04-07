4c637260bf12d1ac2c209513c9a5bdc55c6f7f5c
= <jmeunier@bu.edu>
2016-04-06 22:37:06 -0400

    Custom Task Dialog Box

--- BenchBotGUIMain.py
+++ BenchBotGUIMain.py
@@ -31,11 +31,54 @@ import serial.tools.list_ports
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType('BenchBotMain.ui')
 
-class TaskDialogWindow(QDialog):
+class NewTaskDialogWindow(QDialog):
 
     def __init__(self, parent=None):
-        super(TaskDialogWindow,self).__init__()
+        super(NewTaskDialogWindow,self).__init__()
+        Ui_Dialog, QtBaseClass = uic.loadUiType('CreateCustomTask.ui')
+        self.diag = Ui_Dialog()
+        self.diag.setupUi(self)
+
+        #connect dialog buttons
+
+        self.diag.upload_python.clicked.connect(self.upload_python_clicked)
+        self.diag.upload_ui.clicked.connect(self.upload_ui_clicked)
+        self.diag.upload_json.clicked.connect(self.upload_json_clicked)
+
+    def upload_python_clicked(self):
+        file_dialog = QFileDialog()
+        file_dialog.setFileMode(QFileDialog.AnyFile)
+        file = file_dialog.getOpenFileName(self, 'Open Python File', '', "Python Files (*.py)")
+        print ('Path to file is:\n') 
+        print (file)
+
+    def upload_ui_clicked(self):
+        file_dialog = QFileDialog()
+        file_dialog.setFileMode(QFileDialog.AnyFile)
+        file = file_dialog.getOpenFileName(self, 'Open UI File', '', "Ui Files (*.ui)")
+        print ('Path to file is:\n')
+        print (file)
+
+    def upload_json_clicked(self):
+        file_dialog = QFileDialog()
+        file_dialog.setFileMode(QFileDialog.AnyFile)
+        file = file_dialog.getOpenFileName(self, 'Open File', '', "JSON Files (*.json)")
+        print ('Path to file is:\n')
+        print (file)
+
+class PCRTaskDialogWindow(QDialog):
+
+    def __init__(self, parent=None):
+        super(PCRTaskDialogWindow,self).__init__()
         Ui_Dialog, QtBaseClass = uic.loadUiType('TaskDialogBox.ui')
+        self.diag = Ui_Dialog()
+        self.diag.setupUi(self)
+
+class ColonyTaskDialogWindow(QDialog):
+
+    def __init__(self, parent=None):
+        super(ColonyTaskDialogWindow, self).__init__()
+        Ui_Dialog, QtBaseClass = uic.loadUiType('CloningTaskDialogBox.ui')
         self.diag = Ui_Dialog()
         self.diag.setupUi(self)
 
@@ -46,190 +89,6 @@ class WorkSpaceDialog(QDialog):
         Ui_Dialog, QtBaseClass = uic.loadUiType('WorkSpaceConfig.ui')
         self.diag = Ui_Dialog()
         self.diag.setupUi(self)
-
-class ShowFileDialog(QFileDialog):
-
-    def __init__(self,parent=None):
-        super(ShowFileDialog,self).__init__()
-
-class GLWidget(QOpenGLWidget):
-    xRotationChanged = pyqtSignal(int)
-    yRotationChanged = pyqtSignal(int)
-    zRotationChanged = pyqtSignal(int)
-
-    def __init__(self, parent=None):
-        super(GLWidget, self).__init__(parent)
-
-        self.object = 0
-        self.xRot = 0
-        self.yRot = 0
-        self.zRot = 0
-
-        self.lastPos = QPoint()
-
-        self.trolltechGreen = QColor.fromCmykF(0.40, 0.0, 1.0, 0.0)
-        self.trolltechPurple = QColor.fromCmykF(0.39, 0.39, 0.0, 0.0)
-
-    def minimumSizeHint(self):
-        return QSize(50, 50)
-
-    def sizeHint(self):
-        return QSize(400, 400)
-
-    def setXRotation(self, angle):
-        angle = self.normalizeAngle(angle)
-        if angle != self.xRot:
-            self.xRot = angle
-            self.xRotationChanged.emit(angle)
-            self.update()
-
-    def setYRotation(self, angle):
-        angle = self.normalizeAngle(angle)
-        if angle != self.yRot:
-            self.yRot = angle
-            self.yRotationChanged.emit(angle)
-            self.update()
-
-    def setZRotation(self, angle):
-        angle = self.normalizeAngle(angle)
-        if angle != self.zRot:
-            self.zRot = angle
-            self.zRotationChanged.emit(angle)
-            self.update()
-
-    def initializeGL(self):
-        self.gl = self.context().versionFunctions()
-        self.gl.initializeOpenGLFunctions()
-
-        self.setClearColor(self.trolltechPurple.darker())
-        self.object = self.makeObject()
-        self.gl.glShadeModel(self.gl.GL_FLAT)
-        self.gl.glEnable(self.gl.GL_DEPTH_TEST)
-        self.gl.glEnable(self.gl.GL_CULL_FACE)
-
-    def paintGL(self):
-        self.gl.glClear(
-                self.gl.GL_COLOR_BUFFER_BIT | self.gl.GL_DEPTH_BUFFER_BIT)
-        self.gl.glLoadIdentity()
-        self.gl.glTranslated(0.0, 0.0, -10.0)
-        self.gl.glRotated(self.xRot / 16.0, 1.0, 0.0, 0.0)
-        self.gl.glRotated(self.yRot / 16.0, 0.0, 1.0, 0.0)
-        self.gl.glRotated(self.zRot / 16.0, 0.0, 0.0, 1.0)
-        self.gl.glCallList(self.object)
-
-    def resizeGL(self, width, height):
-        side = min(width, height)
-        if side < 0:
-            return
-
-        self.gl.glViewport((width - side) // 2, (height - side) // 2, side,
-                side)
-
-        self.gl.glMatrixMode(self.gl.GL_PROJECTION)
-        self.gl.glLoadIdentity()
-        self.gl.glOrtho(-0.5, +0.5, +0.5, -0.5, 4.0, 15.0)
-        self.gl.glMatrixMode(self.gl.GL_MODELVIEW)
-
-    def mousePressEvent(self, event):
-        self.lastPos = event.pos()
-
-    def mouseMoveEvent(self, event):
-        dx = event.x() - self.lastPos.x()
-        dy = event.y() - self.lastPos.y()
-
-        if event.buttons() & Qt.LeftButton:
-            self.setXRotation(self.xRot + 8 * dy)
-            self.setYRotation(self.yRot + 8 * dx)
-        elif event.buttons() & Qt.RightButton:
-            self.setXRotation(self.xRot + 8 * dy)
-            self.setZRotation(self.zRot + 8 * dx)
-
-        self.lastPos = event.pos()
-
-    def makeObject(self):
-        genList = self.gl.glGenLists(1)
-        self.gl.glNewList(genList, self.gl.GL_COMPILE)
-
-        self.gl.glBegin(self.gl.GL_QUADS)
-
-        x1 = +0.06
-        y1 = -0.14
-        x2 = +0.14
-        y2 = -0.06
-        x3 = +0.08
-        y3 = +0.00
-        x4 = +0.30
-        y4 = +0.22
-
-        self.quad(x1, y1, x2, y2, y2, x2, y1, x1)
-        self.quad(x3, y3, x4, y4, y4, x4, y3, x3)
-
-        self.extrude(x1, y1, x2, y2)
-        self.extrude(x2, y2, y2, x2)
-        self.extrude(y2, x2, y1, x1)
-        self.extrude(y1, x1, x1, y1)
-        self.extrude(x3, y3, x4, y4)
-        self.extrude(x4, y4, y4, x4)
-        self.extrude(y4, x4, y3, x3)
-
-        NumSectors = 200
-
-        for i in range(NumSectors):
-            angle1 = (i * 2 * math.pi) / NumSectors
-            x5 = 0.30 * math.sin(angle1)
-            y5 = 0.30 * math.cos(angle1)
-            x6 = 0.20 * math.sin(angle1)
-            y6 = 0.20 * math.cos(angle1)
-
-            angle2 = ((i + 1) * 2 * math.pi) / NumSectors
-            x7 = 0.20 * math.sin(angle2)
-            y7 = 0.20 * math.cos(angle2)
-            x8 = 0.30 * math.sin(angle2)
-            y8 = 0.30 * math.cos(angle2)
-
-            self.quad(x5, y5, x6, y6, x7, y7, x8, y8)
-
-            self.extrude(x6, y6, x7, y7)
-            self.extrude(x8, y8, x5, y5)
-
-        self.gl.glEnd()
-        self.gl.glEndList()
-
-        return genList
-
-    def quad(self, x1, y1, x2, y2, x3, y3, x4, y4):
-        self.setColor(self.trolltechGreen)
-
-        self.gl.glVertex3d(x1, y1, -0.05)
-        self.gl.glVertex3d(x2, y2, -0.05)
-        self.gl.glVertex3d(x3, y3, -0.05)
-        self.gl.glVertex3d(x4, y4, -0.05)
-
-        self.gl.glVertex3d(x4, y4, +0.05)
-        self.gl.glVertex3d(x3, y3, +0.05)
-        self.gl.glVertex3d(x2, y2, +0.05)
-        self.gl.glVertex3d(x1, y1, +0.05)
-
-    def extrude(self, x1, y1, x2, y2):
-        self.setColor(self.trolltechGreen.darker(250 + int(100 * x1)))
-
-        self.gl.glVertex3d(x1, y1, +0.05)
-        self.gl.glVertex3d(x2, y2, +0.05)
-        self.gl.glVertex3d(x2, y2, -0.05)
-        self.gl.glVertex3d(x1, y1, -0.05)
-
-    def normalizeAngle(self, angle):
-        while angle < 0:
-            angle += 360 * 16
-        while angle > 360 * 16:
-            angle -= 360 * 16
-        return angle
-
-    def setClearColor(self, c):
-        self.gl.glClearColor(c.redF(), c.greenF(), c.blueF(), c.alphaF())
-
-    def setColor(self, c):
-        self.gl.glColor4f(c.redF(), c.greenF(), c.blueF(), c.alphaF())
 
 
 class DobotGUIApp(QMainWindow):
@@ -243,7 +102,7 @@ class DobotGUIApp(QMainWindow):
         # connect to WorkSpace Tab and control GLWidget
 
         
-        self.glWidget = GLWidget()
+        '''self.glWidget = GLWidget()
 
         self.ui.x_vert_slider.valueChanged.connect(self.glWidget.setXRotation)
         self.ui.openGLWidget.xRotationChanged.connect(self.ui.x_vert_slider.setValue)
@@ -255,19 +114,26 @@ class DobotGUIApp(QMainWindow):
 
         self.ui.x_vert_slider.setValue(15 * 16)
         self.ui.y_vert_slider.setValue(345 * 16)
-        self.ui.z_vert_slider.setValue(0 * 16)
+        self.ui.z_vert_slider.setValue(0 * 16)'''
+
+        #conenct menubar QAction File option to a QFileDialog 
+
+        self.ui.new_file_action.triggered.connect(self.file_dialog_clicked)
 
 
         # connect to menubar QAction item options for Task bar Dialog Box
 
-        self.task = TaskDialogWindow()
-        self.ui.new_cloning_task_action.triggered.connect(self.task.show) #want to be able to set window name too depending on what they clicked
+        self.new_task = NewTaskDialogWindow()
+        self.task = PCRTaskDialogWindow()
+        self.task2 = ColonyTaskDialogWindow()
+
+        self.ui.new_cloning_task_action.triggered.connect(self.new_task.show) 
         self.ui.new_pcr_cloning_task_action.triggered.connect(self.task.show)
         self.ui.mike_pcr_cloning_task_action.triggered.connect(self.task.show)
         self.ui.jojo_pcr_cloning_task_action.triggered.connect(self.task.show)
-        self.ui.new_colony_cloning_task_action.triggered.connect(self.task.show)
-        self.ui.mike_colony_cloning_task_action.triggered.connect(self.task.show)
-        self.ui.jojo_colony_cloning_task_action.triggered.connect(self.task.show)
+        self.ui.new_colony_cloning_task_action.triggered.connect(self.task2.show)
+        self.ui.mike_colony_cloning_task_action.triggered.connect(self.task2.show)
+        self.ui.jojo_colony_cloning_task_action.triggered.connect(self.task2.show)
 
         # connect to menubar QAction item options for WorkSpaceConfig Dialog Box
 
@@ -275,6 +141,7 @@ class DobotGUIApp(QMainWindow):
         self.ui.actionNew_Work_Space.triggered.connect(self.work.show)
 
         # connect serial ports list refresh button clicked event to the update serial port list function
+
         self.ui.pushButtonRefreshSerialPortsList.clicked.connect(self.update_serial_port_list)
         # connect move coordinates button clicked event to function to move to the coordinate specified
         self.ui.pushButtonMoveToCoordinate.clicked.connect(self.pushButtonMoveToCoordinate_clicked)
@@ -317,7 +184,12 @@ class DobotGUIApp(QMainWindow):
         self.update_serial_port_list()
 
     
+    def file_dialog_clicked(self):
 
+        file_dialog = QFileDialog()
+        file_dialog.setFileMode(QFileDialog.AnyFile)
+        file = file_dialog.getOpenFileName(self, 'Open File')
+        print ('Path to file is:\n'), (file) 
 
     def pushButtonMoveToCoordinate_clicked(self):
 
@@ -835,7 +707,184 @@ class DobotGUIApp(QMainWindow):
             msg.setWindowTitle(windowTitle)
             msg.exec()
 
+class GLWidget(QOpenGLWidget):
+    xRotationChanged = pyqtSignal(int)
+    yRotationChanged = pyqtSignal(int)
+    zRotationChanged = pyqtSignal(int)
 
+    def __init__(self, parent=None):
+        super(GLWidget, self).__init__(parent)
+
+        self.object = 0
+        self.xRot = 0
+        self.yRot = 0
+        self.zRot = 0
+
+        self.lastPos = QPoint()
+
+        self.trolltechGreen = QColor.fromCmykF(0.40, 0.0, 1.0, 0.0)
+        self.trolltechPurple = QColor.fromCmykF(0.39, 0.39, 0.0, 0.0)
+
+    def minimumSizeHint(self):
+        return QSize(50, 50)
+
+    def sizeHint(self):
+        return QSize(400, 400)
+
+    def setXRotation(self, angle):
+        angle = self.normalizeAngle(angle)
+        if angle != self.xRot:
+            self.xRot = angle
+            self.xRotationChanged.emit(angle)
+            self.update()
+
+    def setYRotation(self, angle):
+        angle = self.normalizeAngle(angle)
+        if angle != self.yRot:
+            self.yRot = angle
+            self.yRotationChanged.emit(angle)
+            self.update()
+
+    def setZRotation(self, angle):
+        angle = self.normalizeAngle(angle)
+        if angle != self.zRot:
+            self.zRot = angle
+            self.zRotationChanged.emit(angle)
+            self.update()
+
+    def initializeGL(self):
+        self.gl = self.context().versionFunctions()
+        self.gl.initializeOpenGLFunctions()
+
+        self.setClearColor(self.trolltechPurple.darker())
+        self.object = self.makeObject()
+        self.gl.glShadeModel(self.gl.GL_FLAT)
+        self.gl.glEnable(self.gl.GL_DEPTH_TEST)
+        self.gl.glEnable(self.gl.GL_CULL_FACE)
+
+    def paintGL(self):
+        self.gl.glClear(
+                self.gl.GL_COLOR_BUFFER_BIT | self.gl.GL_DEPTH_BUFFER_BIT)
+        self.gl.glLoadIdentity()
+        self.gl.glTranslated(0.0, 0.0, -10.0)
+        self.gl.glRotated(self.xRot / 16.0, 1.0, 0.0, 0.0)
+        self.gl.glRotated(self.yRot / 16.0, 0.0, 1.0, 0.0)
+        self.gl.glRotated(self.zRot / 16.0, 0.0, 0.0, 1.0)
+        self.gl.glCallList(self.object)
+
+    def resizeGL(self, width, height):
+        side = min(width, height)
+        if side < 0:
+            return
+
+        self.gl.glViewport((width - side) // 2, (height - side) // 2, side,
+                side)
+
+        self.gl.glMatrixMode(self.gl.GL_PROJECTION)
+        self.gl.glLoadIdentity()
+        self.gl.glOrtho(-0.5, +0.5, +0.5, -0.5, 4.0, 15.0)
+        self.gl.glMatrixMode(self.gl.GL_MODELVIEW)
+
+    def mousePressEvent(self, event):
+        self.lastPos = event.pos()
+
+    def mouseMoveEvent(self, event):
+        dx = event.x() - self.lastPos.x()
+        dy = event.y() - self.lastPos.y()
+
+        if event.buttons() & Qt.LeftButton:
+            self.setXRotation(self.xRot + 8 * dy)
+            self.setYRotation(self.yRot + 8 * dx)
+        elif event.buttons() & Qt.RightButton:
+            self.setXRotation(self.xRot + 8 * dy)
+            self.setZRotation(self.zRot + 8 * dx)
+
+        self.lastPos = event.pos()
+
+    def makeObject(self):
+        genList = self.gl.glGenLists(1)
+        self.gl.glNewList(genList, self.gl.GL_COMPILE)
+
+        self.gl.glBegin(self.gl.GL_QUADS)
+
+        x1 = +0.06
+        y1 = -0.14
+        x2 = +0.14
+        y2 = -0.06
+        x3 = +0.08
+        y3 = +0.00
+        x4 = +0.30
+        y4 = +0.22
+
+        self.quad(x1, y1, x2, y2, y2, x2, y1, x1)
+        self.quad(x3, y3, x4, y4, y4, x4, y3, x3)
+
+        self.extrude(x1, y1, x2, y2)
+        self.extrude(x2, y2, y2, x2)
+        self.extrude(y2, x2, y1, x1)
+        self.extrude(y1, x1, x1, y1)
+        self.extrude(x3, y3, x4, y4)
+        self.extrude(x4, y4, y4, x4)
+        self.extrude(y4, x4, y3, x3)
+
+        NumSectors = 200
+
+        for i in range(NumSectors):
+            angle1 = (i * 2 * math.pi) / NumSectors
+            x5 = 0.30 * math.sin(angle1)
+            y5 = 0.30 * math.cos(angle1)
+            x6 = 0.20 * math.sin(angle1)
+            y6 = 0.20 * math.cos(angle1)
+
+            angle2 = ((i + 1) * 2 * math.pi) / NumSectors
+            x7 = 0.20 * math.sin(angle2)
+            y7 = 0.20 * math.cos(angle2)
+            x8 = 0.30 * math.sin(angle2)
+            y8 = 0.30 * math.cos(angle2)
+
+            self.quad(x5, y5, x6, y6, x7, y7, x8, y8)
+
+            self.extrude(x6, y6, x7, y7)
+            self.extrude(x8, y8, x5, y5)
+
+        self.gl.glEnd()
+        self.gl.glEndList()
+
+        return genList
+
+    def quad(self, x1, y1, x2, y2, x3, y3, x4, y4):
+        self.setColor(self.trolltechGreen)
+
+        self.gl.glVertex3d(x1, y1, -0.05)
+        self.gl.glVertex3d(x2, y2, -0.05)
+        self.gl.glVertex3d(x3, y3, -0.05)
+        self.gl.glVertex3d(x4, y4, -0.05)
+
+        self.gl.glVertex3d(x4, y4, +0.05)
+        self.gl.glVertex3d(x3, y3, +0.05)
+        self.gl.glVertex3d(x2, y2, +0.05)
+        self.gl.glVertex3d(x1, y1, +0.05)
+
+    def extrude(self, x1, y1, x2, y2):
+        self.setColor(self.trolltechGreen.darker(250 + int(100 * x1)))
+
+        self.gl.glVertex3d(x1, y1, +0.05)
+        self.gl.glVertex3d(x2, y2, +0.05)
+        self.gl.glVertex3d(x2, y2, -0.05)
+        self.gl.glVertex3d(x1, y1, -0.05)
+
+    def normalizeAngle(self, angle):
+        while angle < 0:
+            angle += 360 * 16
+        while angle > 360 * 16:
+            angle -= 360 * 16
+        return angle
+
+    def setClearColor(self, c):
+        self.gl.glClearColor(c.redF(), c.greenF(), c.blueF(), c.alphaF())
+
+    def setColor(self, c):
+        self.gl.glColor4f(c.redF(), c.greenF(), c.blueF(), c.alphaF())
 
 # main function
 if __name__ == '__main__':

f8ff7e23923823647da66b0b0753348743108f65
= <jmeunier@bu.edu>
2016-04-14 18:44:03 -0400

    can only draw one cube

--- drawShapes.py
+++ drawShapes.py
@@ -2,8 +2,8 @@
 
 Author: JoJo Meunier jmeunier@bu.edu 4/10/16
 
-python script using pyopengl to draw shapes 
-this will be used to model the workspace in 3D on an openGL widget in pyqt
+pself.ython script using pself.yopengl to draw shapes 
+this will be used to model the workspace in 3D on an openGL widget in pself.yqt
 
 '''
 
@@ -21,17 +21,7 @@ from PyQt5.QtGui import QColor
 import sys
 import json
 
-verticies = (
-		(1,-1,-1),
-		(1,1,-1),
-		(-1,1,-1),
-		(-1,-1,-1),
-		(1,-1,1),
-		(1,1,1),
-		(-1,-1,1),
-		(-1,1,1)
-		)
-edges = (
+edges = ( #in order to draw a cube we will always connect our edges in this way
 		(0,1),
 		(0,3),
 		(0,4),
@@ -48,23 +38,60 @@ edges = (
 
 class glWidget(QGLWidget):
     
-    def __init__(self, parent = None):
+    def __init__(self, dimensions, location, parent = None):
         super(glWidget, self).__init__(parent)
+        
+        #get shape dimensions for the cubes we want to draw:
 
-    def paintGL(self):
+        self.x = int(dimensions["width"]) / 20 #scale by twenty to render 
+        self.y = int(dimensions["height"]) / 20
+        self.z = int(dimensions["length"]) / 20 
 
-    	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    	glBegin(GL_LINES)
-    	for edge in edges:
-    		for vertex in edge:
-    			glVertex3fv(verticies[vertex])
-    	glEnd()
+        #get coordinate locations for the cube we want to draw:
 
-    '''def resizeGL(self, w, h):
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        glOrtho(-50, 50, -50, 50, -50.0, 50.0)
-        glViewport(0, 0, w, h)'''
+
+
+        #define boundaries for where we can draw the cube:
+
+        if location["y"] is not 0.0: #we do not want to break the Y plane everything should be flat on a table
+        	location["y"] == 0.0
+
+        if location["z"] > -3.0: # any closer than -3.0 and the shape will be too close to render 
+        	location["z"] == -3.0
+		
+        #boundaries by which cube is bounded by x coordinate
+        if location["x"] > 4.0: 
+        	location["x"] == 4.0
+        elif location["x"] < -4.0:
+        	location["x"] == -4.0
+        
+        self.locationX = location["x"]
+        self.locationY = location["y"]
+        self.locationZ =location["z"]
+
+        print(self.locationX,self.locationY,self.locationZ)
+
+    def paintGL(self, Width=640,Height=480):
+
+    #define the verticies of the cube to be drawn:
+	    verticies = (
+	    	(self.x,-self.y,-self.z),
+	    	(self.x,self.y,-self.z),
+	    	(-self.x,self.y,-self.z),
+	    	(-self.x,-self.y,-self.z),
+	    	(self.x,-self.y,self.z),
+	    	(self.x,self.y,self.z),
+	    	(-self.x,-self.y,self.z),
+	    	(-self.x,self.y,self.z)
+	    	)
+	    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+	    glBegin(GL_LINES)
+	    for edge in edges:
+	    	for vertex in edge:
+	    		glVertex3fv(verticies[vertex])
+
+	    glEnd()
+
 
     def initializeGL(self, Width=640, Height=480):
         glClearColor(0.0, 0.0, 0.0, 0.0)
@@ -74,9 +101,10 @@ class glWidget(QGLWidget):
         glShadeModel(GL_SMOOTH)   
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
-        gluPerspective(80.0, float(Width)/float(Height), 0.1, 50.0)
-        glTranslatef(0.0,0.0,-3.0)
-        glRotatef(45.0,30.0,0,-20)
+        gluPerspective(45.0, float(Width)/float(Height), 1, 50.0)
+        glTranslatef(self.locationX,self.locationY,self.locationZ)
+        #glTranslatef(self.locationX,self.locationY,self.locationZ)
+        glRotatef(0.0,0,0,-20)
         glMatrixMode(GL_MODELVIEW)
 
 if __name__ == '__main__':
